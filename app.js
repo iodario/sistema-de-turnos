2930e848a24e816b3941693ac173dada43c6e69d
Dario Turchi <iodariomdq@outlook.com>
2022-05-22 22:03:05 -0300

    agrego validaciones, quito extends

--- app.js
+++ app.js
@@ -1,42 +1,100 @@
 /* 
-* sistema de clinica medica*
-* login de usuarios, reserva de turnos y cancelaciones *
+* Sistema de clinica medica*
+* Solicito a los usuarios ingrese sus datos y un turno deseado. *
+* Con validacion de ingreso de datos ingresados por el usuario.
+* Muestro un listado de usuarios y sus turnos por consola.
 */
 
 class Usuario {
-    constructor(nombre, edad, dni) {
-        this.nombre = nombre;
+    constructor(nombre, edad, dni, turno) {
+        this.nombre = nombre.toLowerCase();
         this.edad = parseInt(edad);
         this.dni = parseInt(dni);
-        this.turno = false;
-    }
-    reservarTurno() {
-        this.turno = true;
+        this.turno = new Date(turno);
     }
 }
 
-class Turno extends Usuario{
-    constructor(nombre,dia,horario){
-        super(nombre);      
-        this.dia = dia;
-        this.horario = horario;
-    }
-}
-
-let bienvenido = confirm ("Bienvenido a sistema de turnos. Desea comenzar?")
+let bienvenido = confirm("Bienvenido a sistema de turnos. Desea comenzar?")
 const usuarios = [];
-const turnos = [];
-let nuevoTurno = "";
 
-while(bienvenido){
-usuarios.push(new Usuario(
-    prompt("ingrese su nombre"), prompt("ingrese su edad"), prompt("ingrese su dni")
-));
-turnos.push(new Turno(
-    this.nombre, prompt("ingrese un dia de la semana"), prompt("ingrese horario mañana o tarde")
-));
-
-bienvenido = confirm ("Desea reservar otro turno? \nCancelar para finalizar.")
+//invalidDate() elimina el ultimo dato de usuario ingresado incorrectamente, y finaliza el programa.
+function invalidDate() {
+    usuarios.pop();
+    bienvenido = false;
+    console.log(usuarios);
 }
 
+while (bienvenido) {
+//solicito datos al usuario
+    usuarios.push(new Usuario(
+        prompt("Ingrese su nombre"),
+        prompt("Ingrese su edad"),
+        prompt("Ingrese su dni"),
+        prompt("Ingrese turno deseado. \nFormato:  mes/dia/año  hora:minutos.")
+    ));
+
+
+//valido inputs, si ALGUNO falla llamo a la funcion invalidDate()
+    if (usuarios.some((user)=> (isNaN(user.edad)))) {
+        alert("Usted a ingresado una EDAD inválida. Reintente.");
+        invalidDate()
+
+    } else if (usuarios.some((user)=> (isNaN(user.dni)))) {
+        alert("Usted a ingresado un DNI inválido. Reintente.");
+        invalidDate()
+
+    } else if (usuarios.some((user)=> (isNaN(Date.parse(user.turno)))) ) {
+    // } else if (isNaN(Date.parse(usuarios[0].turno))) {
+        alert("Usted a ingresado una fecha inválida. \nFormato:  mes/dia/año  hora:minutos.");
+        invalidDate()
+
+    } else {
+        bienvenido = confirm("Turno reservado con éxito. \nDesea reservar otro turno?")
+    }
+
+}
+
+usuarios.forEach((user) => console.log(user));
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Otras opciones */
+
+/* if (isNaN(usuarios[0].edad)) {
+    alert("Usted a ingresado una edad inválida. Reintente.");
+    invalidDate()
+
+} else if (isNaN(usuarios[0].dni)) {
+    alert("Usted a ingresado un dni inválido");
+    invalidDate()
+
+} else if (isNaN(Date.parse(usuarios[0].turno))) {
+    alert("Usted a ingresado una fecha inválida. \nFormato:  mes/dia/año  hora:minutos.");
+    invalidDate()
+
+} else {
+    bienvenido = confirm("Turno reservado con éxito. \nDesea reservar otro turno?")
+} */
+
+
+
+/* if (isNaN(usuarios[0].edad) || isNaN(usuarios[0].dni) || isNaN(Date.parse(usuarios[0].turno))) {
+    alert("usted a ingresado un dato inválido");
+    usuarios.pop()
+    bienvenido = false;
+    console.log(usuarios);
+} else {
+    bienvenido = confirm("Turno reservado con éxito. \nDesea reservar otro turno? \nCancelar para finalizar.")
+}
+ */
 
